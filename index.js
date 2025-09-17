05958fd0f9b5367a94175617b05f8a352ce35f5a
Zilong Guan <cerealbox269@gmail.com>
2025-09-17 18:24:49 -0400

    feat(validation): added working validation logic

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 const defaultSection = document.getElementById('default-section');
 const success = document.getElementById('success');
-const subscribeBtn = document.getElementById('subscribe-btn');
+// const subscribeBtn = document.getElementById('subscribe-btn');
 const dismissBtn = document.getElementById('dismiss-btn');
 
-function toggleHidden(e) {
-  e.preventDefault();
-
+function toggleHidden() {
   defaultSection.classList.toggle('hidden');
   success.classList.toggle('hidden');
 }
 
-// Fn: apply form logic & authentication
-// If pass, use toggleHidden.fn
+// function validateEmail(e) {
 
-// Possibly turn these into just 1 button ?
-subscribeBtn.addEventListener('click', toggleHidden);
+// }
+
+// Fn: Form submitted
+const form = document.getElementById('form');
+const emailInput = document.getElementById('email');
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // Get input
+  const email = emailInput.value;
+  console.log(email);
+
+  // Check if input matches Regex:
+  const validation = emailRegex.test(email);
+
+  if (validation) {
+    console.log('Submitted');
+    toggleHidden();
+  } else {
+    console.log('Error!');
+  }
+  // If success, log submitted and run toggleHidden
+});
+
+// Revert success page
 dismissBtn.addEventListener('click', toggleHidden);
