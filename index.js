10fb6066c4213ecc8638fde713f8b1b583b48d8c
Zilong Guan <cerealbox269@gmail.com>
2025-09-17 23:18:24 -0400

    refactor(validation): created separate handleValidation function for readability and cleaned up comments

--- index.js
+++ index.js
@@ -1,28 +1,22 @@
 const defaultSection = document.getElementById('default-section');
 const success = document.getElementById('success');
-// const subscribeBtn = document.getElementById('subscribe-btn');
 const dismissBtn = document.getElementById('dismiss-btn');
 const confirmationEmail = document.getElementById('confirmation-email');
+const form = document.getElementById('form');
+const emailInput = document.getElementById('email');
+const errorMsg = document.getElementById('error-msg');
 
 function toggleHidden() {
   defaultSection.classList.toggle('hidden');
   success.classList.toggle('hidden');
 }
 
-// function handleValidation(e) {
-
-// }
-
-const form = document.getElementById('form');
-const emailInput = document.getElementById('email');
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const errorMsg = document.getElementById('error-msg');
-
-form.addEventListener('submit', (e) => {
+function handleValidation(e) {
   e.preventDefault();
-  const email = emailInput.value;
 
-  // Check if input matches Regex:
+  const email = emailInput.value;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Matches as long as no whitespace or extra @ symbols
+
   const validation = emailRegex.test(email);
 
   if (validation) {
@@ -35,7 +29,7 @@ form.addEventListener('submit', (e) => {
     errorMsg.classList.remove('hidden');
     emailInput.classList.add('form__input--error');
   }
-});
+}
 
-// Revert success page
-dismissBtn.addEventListener('click', toggleHidden);
+form.addEventListener('submit', handleValidation);
+dismissBtn.addEventListener('click', toggleHidden); // Reverts page
